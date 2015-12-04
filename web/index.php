f48958bc7a72644fbfe11d0457b43090fde92fa7
nAndy <nandy@wikia-inc.com>
2015-12-04 09:32:23 +0100

    used SilexSessionProvider and added login&logout

--- web/index.php
+++ web/index.php
@@ -1,29 +1,53 @@
 <?php
 require_once(__DIR__ . '/../vendor/autoload.php');
+require_once(__DIR__ . '/../models/User.class.php');
 
 $app = new Silex\Application();
-
 $app['debug'] = true;
-$loggedIn = true;
 
 // Providers
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
+$app->register(new Silex\Provider\TwigServiceProvider(), [
    'twig.path' => __DIR__ . '/../views',
-));
+]);
+$app->register(new Silex\Provider\SessionServiceProvider());
+
+use Symfony\Component\HttpFoundation\Request;
+use FriendlyGifts\User;
+
+$user = $app['session']->get('user');
 
 // Controllers
-$app->get('/', function () use ($app, $loggedIn) {
-   if( $loggedIn !== true ) {
+$app->get('/', function () use ($app, $user) {
+   if( null === $user ) {
       return $app->redirect('/login');
    }
 
    return $app['twig']->render('dashboard.twig', [
-       'name' => 'nAndy'
+       'name' => $user->getName()
    ]);
 });
 
 $app->get('/login', function () use ($app) {
-   return $app['twig']->render('login.twig');
+   return $app['twig']->render('login.twig', [ 'error' => '' ]);
+});
+
+$app->post('/login', function (Request $request) use ($app) {
+   $user = new User\User($request->get('login'), $request->get('password'));
+   $user->login();
+
+   if( $user->isLoggedIn() ) {
+      $app['session']->set('user', $user);
+      return $app->redirect('/');
+   }
+
+   return $app['twig']->render('login.twig', [
+      'error' => 'Nieprawidłowy login lub hasło.'
+   ]);
+});
+
+$app->get('/logout', function () use ($app) {
+   $app['session']->set('user', null);
+   return $app->redirect('/');
 });
 
 $app->run();
